d8d013150902cab648cf882968c4f724a928f979
Mohamed Cherkaoui <chermed@gmail.com>
2017-12-03 13:20:25 +0100

    Fix exit on log

--- dyools.py
+++ dyools.py
@@ -14,7 +14,7 @@ try:
 except NameError:
     basestring = str
 
-__VERSION__ = '0.4.2'
+__VERSION__ = '0.4.3'
 __AUTHOR__ = ''
 __WEBSITE__ = ''
 __DATE__ = ''
@@ -195,24 +195,42 @@ class Logger(object):
                     pass
         return msg
 
-    def info(self, msg):
+    def info(self, msg, exit=False):
         click.echo(self._clean_msg(msg))
+        if exit:
+            sys.exit(-1)
 
-    def warn(self, msg):
+    def warn(self, msg, exit=False):
         click.secho(self._clean_msg(msg), fg='yellow')
+        if exit:
+            sys.exit(-1)
 
-    def debug(self, msg):
+    def debug(self, msg, exit=False):
         click.secho(self._clean_msg(msg), fg='blue')
+        if exit:
+            sys.exit(-1)
 
-    def success(self, msg):
+    def success(self, msg, exit=False):
         click.secho(self._clean_msg(msg), fg='green')
+        if exit:
+            sys.exit(-1)
 
-    def code(self, msg):
+    def code(self, msg, exit=False):
         click.secho(self._clean_msg(msg), fg='cyan')
+        if exit:
+            sys.exit(-1)
 
-    def error(self, msg):
+    def error(self, msg, exit=True):
         click.secho(self._clean_msg(msg), fg='red')
-        sys.exit(-1)
+        if exit:
+            sys.exit(-1)
 
-    def title(self, msg):
+    def error(self, msg, exit=False):
+        click.secho(self._clean_msg(msg), fg='red')
+        if exit:
+            sys.exit(-1)
+
+    def title(self, msg, exit=False):
         click.secho(self._clean_msg(msg), fg='white', bold=True)
+        if exit:
+            sys.exit(-1)
