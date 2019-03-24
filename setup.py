5e1c08095daff539eb3861477aff6d0ef5240021
Mohamed Cherkaoui <chermed@gmail.com>
2019-03-24 22:35:03 +0100

    To publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = '0.11.0'
+version = '0.12.0'
 
 setup(
     name='dyools',
